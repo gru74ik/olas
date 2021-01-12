fffe070b886b395b6106e2438496d7069d01af4e
Rodion Cherepov <gru74ik@gmail.com>
2021-01-12 21:38:17 +0300

    Rework main.cpp to create window using glfw

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
@@ -7,6 +8,34 @@ int main()
         << "Hey, Zeus!\n"
         << add(72.1f, 73.8f)
         << "\n\n";
+
+
+    GLFWwindow * window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
 
     return 0;
 }
